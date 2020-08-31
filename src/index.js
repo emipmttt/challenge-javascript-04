16b040004f86178883a441611a096ca9f56f748f
Emiliano Pamont <emipmttt@gmail.com>
2020-08-31 18:41:05 -0500

    Solved Challenge :p

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const trialDivision = (number) => {
-    
+
+  if (!Number.isInteger(number)) return false
+
+  if (number < 1 || number == 0 || number == 1 || number == 4) return false;
+
+  var counter = 2
+
+  while (counter < number) {
+    if ((number % counter) == 0) {
+      return false;
+    }
+    counter++
   }
 
-  module.exports = trialDivision;
+  return true
+
+}
+
+module.exports = trialDivision;
